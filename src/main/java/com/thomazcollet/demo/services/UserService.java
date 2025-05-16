1e3501d019ace0f44d97f551cf9cb5717f62a130
Thomaz de aquino collet e silva <tcollet98@hotmail.com>
2025-05-15 22:43:02 -0300

    Created_Users_Services

---
+++ src/main/java/com/thomazcollet/demo/services/UserService.java
@@ -0,0 +1,59 @@
+package com.thomazcollet.demo.services;
+
+import org.apache.tomcat.jni.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.thomazcollet.demo.repositories.TasksRepository;
+import com.thomazcollet.demo.repositories.UsersRepository;
+import com.thomazcollet.demo.models.*;
+
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
+
+@Service
+public class UserService {
+
+    // Atributos
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Autowired
+    private TasksRepository tasksRepository;
+
+    // Métodos
+
+    public Users findById(Long id) {
+        Optional <Users> users = this.usersRepository.findById(id);
+        return users.orElseThrow(() -> new RuntimeException(
+            "Usuario não encontrado!" + " Id:" + id + ", Tipo: " + Users.class.getName()
+        ));
+    }
+
+    @Transactional
+    public Users create (Users obj){
+        obj.setId(null);
+        obj = this.usersRepository.save(obj);
+        this.tasksRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public Users update (Users obj){
+        Users newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.usersRepository.save(newObj);
+    }
+
+    public void delete(Long id){
+        findById(id);
+        System.out.println("Usuário deletado com sucesso!");
+        try {
+            this.usersRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possivel deletar pois há entidades relacionadas!");
+        }
+    }
+}
